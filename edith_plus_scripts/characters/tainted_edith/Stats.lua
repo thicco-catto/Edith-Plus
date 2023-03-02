d95fe5b001f5d33d1d51552de0c44b97947e1d12
thicco-catto <cristobal.martin01@gmail.com>
2023-03-02 15:53:04 +0100

    Tweaked stats

--- edith_plus_scripts/characters/tainted_edith/Stats.lua
+++ edith_plus_scripts/characters/tainted_edith/Stats.lua
@@ -7,12 +7,20 @@ local Constants = require("edith_plus_scripts.Constants")
 function Stats:OnCache(player, cache)
     if player:GetPlayerType() ~= Constants.TAINTED_EDITH_PLAYER then return end
 
+    if (cache & CacheFlag.CACHE_SPEED == CacheFlag.CACHE_SPEED) then
+        player.MoveSpeed = player.MoveSpeed + 1
+    end
+
     if (cache & CacheFlag.CACHE_LUCK == CacheFlag.CACHE_LUCK) then
         player.Luck = player.Luck - 1
     end
 
     if (cache & CacheFlag.CACHE_TEARFLAG == CacheFlag.CACHE_TEARFLAG) then
         player.TearFlags = player.TearFlags | TearFlags.TEAR_BURN
+    end
+
+    if (cache & CacheFlag.CACHE_FIREDELAY == CacheFlag.CACHE_FIREDELAY) then
+        player.MaxFireDelay = player.MaxFireDelay - 2
     end
 end
 EdithPlusMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Stats.OnCache)
